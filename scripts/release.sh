7aabdd189d7045daf8fda8f922914cf6c6efa40a
rafaelcalpena <rafael.calpena@gmail.com>
2019-09-12 16:16:59 -0300

    fix: use build:lib instead of build

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ echo "Changing directory to $TRAVIS_BUILD_DIR"
 # note: This does not work with symlinks
 cd "$TRAVIS_BUILD_DIR"
 
-if npm "run" "build" ; then
+if npm "run" "build:lib" ; then
 	npm "run" "semantic-release"
 else
 	echo "npm run build failed"
